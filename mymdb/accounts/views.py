d3943aadd42b966bcbdd9dc6d4beb408ee8e7b45
YANIV-PC\Yaniv <yanivraveh@gmail.com>
2025-08-16 21:14:31 +0300

    Refactor user signup process and enhance form handling
    - Replaced manual user signup logic with Django's UserCreationForm for improved validation and error handling.
    - Updated signup template to dynamically render form fields and display error messages.
    - Added CSS class for error messages to enhance user feedback during signup.

--- mymdb/accounts/views.py
+++ mymdb/accounts/views.py
@@ -2,29 +2,20 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import  authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 def user_signup(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        password2 = request.POST.get('password2')
-
-        if password != password2:
-            messages.error(request, 'Passwords do not match.')
-            return redirect('accounts:user_signup')
-
-        if User.objects.filter(username=username).exists():
-            messages.error(request, 'Username already taken.')
-            return redirect('accounts:user_signup')
-
-        user = User.objects.create_user(username=username, email=email, password=password)
-        login(request, user)
-        return redirect('/')
-
-    return render(request, 'accounts/signup.html')
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
 
 def user_login(request):
     if request.method == "POST":
