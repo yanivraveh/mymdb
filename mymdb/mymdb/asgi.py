04ffb167f843382c15228a34cc835cbd9909dd53
YANIV-PC\Yaniv <yanivraveh@gmail.com>
2025-10-31 20:42:32 +0200

    Updates asgi.py with a `ProtocolTypeRouter` and a `chat.routing` URLRouter

--- mymdb/mymdb/asgi.py
+++ mymdb/mymdb/asgi.py
@@ -8,9 +8,18 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mymdb.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(chat.routing.websocket_urlpatterns)
+    ),
+})
